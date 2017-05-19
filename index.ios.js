4bea1828596754e1b29d4d269f7d0be120ee1631
Shi Liu <liushie@gmail.com>
2017-05-19 15:07:59 +0800

    initial commit

---
+++ index.ios.js
@@ -0,0 +1,50 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import { TabNavigator } from 'react-navigation';
+
+const MyApp = TabNavigator({
+  Home: {
+    screen: View,
+  }
+})
+
+export default class AwesomeProject extends Component {
+  render() {
+    return (
+      <MyApp />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
